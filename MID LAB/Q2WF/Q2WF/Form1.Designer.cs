d0cae079aace92cc0f4f78f94ea63f32ca2fb2c4
Maryam Amjad <87093456+MaryamAmjad2@users.noreply.github.com>
2023-10-26 15:20:39 +0500

    Add files via upload

---
+++ MID LAB/Q2WF/Q2WF/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Q2WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonValidate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(98, 57);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(128, 20);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(107, 135);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(35, 13);
+            this.labelResult.TabIndex = 1;
+            this.labelResult.Text = "label1";
+            // 
+            // buttonValidate
+            // 
+            this.buttonValidate.Location = new System.Drawing.Point(30, 92);
+            this.buttonValidate.Name = "buttonValidate";
+            this.buttonValidate.Size = new System.Drawing.Size(199, 23);
+            this.buttonValidate.TabIndex = 2;
+            this.buttonValidate.Text = "Parse";
+            this.buttonValidate.UseVisualStyleBackColor = true;
+            this.buttonValidate.Click += new System.EventHandler(this.buttonValidate_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 169);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonValidate);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonValidate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MID LAB/Q2WF/Q2WF/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q2WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+            string input = textBoxInput.Text;
+            List<string> tokens = new List<string>(input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries));
+            int tokenIndex = 0;
+
+            if (ParseList(tokens, ref tokenIndex))
+            {
+                labelResult.Text = "Parsing Successfull.";
+            }
+            else
+            {
+                labelResult.Text = "Parssing Failed.";
+            }
+        }
+
+        private bool ParseList(List<string> tokens, ref int tokenIndex)
+        {
+            return ParseItem(tokens, ref tokenIndex) && ParseRest(tokens, ref tokenIndex);
+        }
+
+        private bool ParseRest(List<string> tokens, ref int tokenIndex)
+        {
+            if (tokenIndex < tokens.Count && tokens[tokenIndex] == ",")
+            {
+                tokenIndex++;
+                return ParseItem(tokens, ref tokenIndex) && ParseRest(tokens, ref tokenIndex);
+            }
+            return true; // ε (empty) production
+        }
+
+        private bool ParseItem(List<string> tokens, ref int tokenIndex)
+        {
+            if (tokenIndex < tokens.Count && (tokens[tokenIndex] == "id" || tokens[tokenIndex] == "num" || tokens[tokenIndex] == "string"))
+            {
+                tokenIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        private void buttonValidate_Click_1(object sender, EventArgs e)
+        {
+            string input = textBoxInput.Text;
+            List<string> tokens = new List<string>(input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries));
+            int tokenIndex = 0;
+
+            if (ParseList(tokens, ref tokenIndex))
+            {
+                labelResult.Text = "Parsing Successful.";
+            }
+            else
+            {
+                labelResult.Text = "Parsing Failed.";
+            }
+        }
+    }
+}
+ 
